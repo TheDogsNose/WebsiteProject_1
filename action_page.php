4d20a4b7b78e5caaeb1737125bcd1b13d4d8e514
TheDogsNose <94315145+TheDogsNose@users.noreply.github.com>
2021-11-16 03:02:33 +0300

    Form done

---
+++ action_page.php
@@ -0,0 +1,15 @@
+<!doctype HTML>
+<html>
+    <head>
+        <title>Hotel Booking</title>
+        <link rel="stylesheet" href="index.css" />
+        <?php
+        echo $_Book["name1"];
+        ?>
+    </head>
+
+    <body>
+        <h1>DONE</h1>
+    </body>
+    
+</html>
